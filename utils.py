3f50efb75ededfb617c7d3bad12f3ee2b800b89d
Jacob Chudnovsky <jchudnov@jacobs-mbp.lan>
2020-05-10 17:40:36 -0700

    add params to agent, add technical indicators setup using talib

--- utils.py
+++ utils.py
@@ -3,13 +3,53 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
+'''
+Tinker Source #1:
+
+a) which indicators to use
+b) time ranges for indicators
+'''
+
+technical_indicators = [
+	('sma', 15), 
+	('sma', 60), 
+	('ema', 30),
+	('rsi', 14), 
+	('atr', 14),
+	('roc', 10),
+	('macd', [12, 26, 9]), #returns macd, macdsignal, macdhist in talib
+	('stoch', [21, 7, 7]) #returns slowk, slowd in talib
+	#more indicators here
+]
+
 
 def get_data(col='close'):
     """ Returns a n x n_step array """
     stock_values = []
     for stock_csv in os.listdir('data/'):
-        stock_data = pd.read_csv('data/{}'.format(stock_csv), usecols=[col])
-        stock_values.append(stock_data[col].values[::-1])
+        stock_data = pd.read_csv('data/{}'.format(stock_csv))
+        curr_stock_value = stock_data.values[::-1]
+	
+	inputs = {
+    	    'open': curr_stock_value[:, 0], 
+	    'high': curr_stock_value[:, 1],
+	    'low': curr_stock_value[:, 2],
+	    'close': curr_stock_value[:, 3],
+	    'volume': curr_stock_value[:, 4]
+	}
+ 
+	for indicator in technical_indicators:
+		indicator_data = None
+		if(indicator[0] == 'macd'):
+			macd, macdsignal, macdhist = Function(indicator[0])(inputs['close'], 
+				fastperiod=indicator[1][0], 
+				slowperiod=indicator[1][1], 
+				signalperiod=indicator[1][2])
+					
+
+	#just get the close column - TEMP
+	stock_values.append(curr_stock_value[:, 3])
+	
 
     # recent price are at top; reverse it
     return np.array(stock_values)
@@ -32,6 +72,7 @@ def get_scaler(env):
 
     scaler = StandardScaler()
     scaler.fit([low, high])
+    print("Scaler is {}".format(scaler))
     return scaler
 
 
